a9602cb9d872ef7abcf67372b26354464a473621
Jakob C <20375203+jakobchis@users.noreply.github.com>
2020-05-05 19:17:22 -0400

    Updated responsiveness on mobile, changed profile image

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.27c2d56684bbde10a3ec957e57ec6d5f.js"
+  "/precache-manifest.3959772b2ab9f2a349cd37949b35a317.js"
 );
 
 self.addEventListener('message', (event) => {
